e4872309e90e71eae811fbb3875e25dc035da265
Rahul Kumar <rahul.kumar0850@gmail.com>
2022-10-04 13:53:05 +0530

    Code Coverage Report

--- src/test/java/com/tdd/TddDemoProjectApplicationTests.java
+++ src/test/java/com/tdd/TddDemoProjectApplicationTests.java
@@ -1,14 +1,8 @@
 package com.tdd;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TddDemoProjectApplicationTests {
-
-	@Test
-	public void test() {
-		
-	}
 
 }
